34989df5c433a0c29a62b2966e23545f81a95c42
iftikharfadjar <fadjar.iftikhar@gmail.com>
2023-08-08 10:42:34 +0000

    add linklist dir

---
+++ linklist/linklist.h
@@ -0,0 +1,18 @@
+#ifndef LINKLIST
+#define LINKLIST
+
+#include <iostream>
+
+using namespace std;
+
+class Linklist {
+	public:
+		void test();
+};
+
+void Linklist::test(){
+	cout << "this test has algo class" << endl;
+}
+
+
+#endif /* UNIT_TEST_UNITTEST_H_ */
---
+++ linklist/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "linklist.h"
+
+using namespace std;
+
+int main(){
+	cout << "Hi World" << endl;
+	Linklist l;
+	l.test();
+	return 0;
+}
